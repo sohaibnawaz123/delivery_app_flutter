1445ed0ad40b2e16147480870f9ac4f041ac2192
Sohaib Nawaz <sohaib.nawaz@mmcgbl.com>
2026-02-13 11:35:46 +0500

    onboarding complete

--- lib/component/app_bar/app_appbar.dart
+++ lib/component/app_bar/app_appbar.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:delivery_app/component/text/content.dart';
-import 'package:delivery_app/core/resource/app_asset.dart';
 import 'package:delivery_app/core/resource/app_color.dart';
 import 'package:delivery_app/core/utils/extension/app_edge_insets.dart';
 import 'package:delivery_app/core/utils/extension/app_navigation.dart';
 import 'package:delivery_app/core/utils/extension/app_padding.dart';
 import 'package:delivery_app/core/utils/extension/app_text_style.dart';
-import 'package:flutter_svg/svg.dart';
 
 class AppAppbar extends StatelessWidget {
   final String title;
